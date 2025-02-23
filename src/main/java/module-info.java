50346c04991f284f3bb641c498079e08792a69ad
Majkl <cerm55@vse.cz>
2025-02-23 10:45:27 +0100

    přidání základního UI do adventury

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,7 @@
 module cz.vse.adventura {
     requires javafx.controls;
     requires javafx.fxml;
-    requires javafx.graphics;
-
-    opens cz.vse.adventura.main to javafx.fxml;
-    opens cz.vse.adventura to javafx.fxml;
 
     exports cz.vse.adventura.main;
-    exports cz.vse.adventura;
+    opens cz.vse.adventura.main to javafx.fxml;
 }
