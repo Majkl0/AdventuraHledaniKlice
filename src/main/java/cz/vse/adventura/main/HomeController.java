e031c94dd955049c49df7b616b4593124ecb2b31
Majkl <cerm55@vse.cz>
2025-02-23 11:40:17 +0100

    vytvoření kontroleru a vypsání vstupu do výstupu

---
+++ src/main/java/cz/vse/adventura/main/HomeController.java
@@ -0,0 +1,19 @@
+package cz.vse.adventura.main;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class HomeController {
+    @FXML
+    private TextArea vystup;
+    @FXML
+private TextField vstup;
+
+@FXML
+    private void odesliVstup(ActionEvent actionEvent) {
+        vystup.appendText(vstup.getText()+"\n");
+        vstup.clear();
+}
+}
