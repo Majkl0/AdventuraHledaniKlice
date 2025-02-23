2a9c3896f0592db09acaed46cb1afa8b52e1ba76
Majkl <cerm55@vse.cz>
2025-02-23 22:21:08 +0100

    video 7 - print úvodního textu

--- src/main/java/cz/vse/adventura/main/HomeController.java
+++ src/main/java/cz/vse/adventura/main/HomeController.java
@@ -1,19 +1,38 @@
 package cz.vse.adventura.main;
 
+import cz.vse.adventura.logika.Hra;
+import cz.vse.adventura.logika.IHra;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
-public class HomeController {
+    public class HomeController {
     @FXML
     private TextArea vystup;
     @FXML
-private TextField vstup;
+    private TextField vstup;
+
+    private IHra hra = new Hra();
+    @FXML
+    private void initialize() {
+        vystup.appendText(hra.vratUvitani()+"\n\n");
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                vstup.requestFocus();
+
+            }
+        });
+    }
 
 @FXML
     private void odesliVstup(ActionEvent actionEvent) {
-        vystup.appendText(vstup.getText()+"\n");
+        String prikaz = vstup.getText();
+        vystup.appendText("> "+prikaz+"\n");
+        String vysledek = hra.zpracujPrikaz(prikaz);
+        vystup.appendText(vysledek+"\n\n");
         vstup.clear();
 }
 }
