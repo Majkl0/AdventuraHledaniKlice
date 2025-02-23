e9bbf3726cd3d60a2626b2744e9e8c508b3c1621
Majkl <cerm55@vse.cz>
2025-02-23 22:34:55 +0100

    video 9 - nastavení UI menu

--- src/main/java/cz/vse/adventura/main/HomeController.java
+++ src/main/java/cz/vse/adventura/main/HomeController.java
@@ -5,11 +5,11 @@ import cz.vse.adventura.logika.IHra;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 
-    public class HomeController {
+import java.util.Optional;
+
+public class HomeController {
         private Button tlacitkoOdesli;
         @FXML
     private TextArea vystup;
@@ -44,4 +44,10 @@ import javafx.scene.control.TextField;
 
         }
 }
-}
+        public void ukoncitHru(ActionEvent actionEvent) {
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Skutečně si přejete ukončit hru?");
+            Optional<ButtonType> result = alert.showAndWait();
+            if (result.isPresent() && result.get() == ButtonType.OK)
+                Platform.exit();
+        }
+    }
