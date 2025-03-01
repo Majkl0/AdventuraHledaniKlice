8855f19ac657ffbb7db743fa2729718800413a1c
Majkl <cerm55@vse.cz>
2025-03-01 10:28:40 +0100

    video 18 - Přidání nápovědy

---
+++ src/main/java/cz/vse/adventura/main/ProstorSouradnice.java
@@ -0,0 +1,11 @@
+package cz.vse.adventura.main;
+
+import javafx.geometry.Point2D;
+
+// Konstanty pro souřadnice prostorů
+final class ProstorSouradnice {
+    static final Point2D CHODBA = new Point2D(251, 126);
+    static final Point2D LOZNICE = new Point2D(159, 34);
+    static final Point2D KUCHYN = new Point2D(416, 57);
+    static final Point2D OBYVAK = new Point2D(288, 262);
+}
